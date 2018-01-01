cfdff765fdfdd5a653bfb4e8104203a05521f51e
zpj <779878443@qq.com>
2018-01-01 22:38:58 +0800

    fix code and add README.md

--- CatchPostCode.py
+++ CatchPostCode.py
@@ -18,7 +18,7 @@ def write_to_file(data_list):
     try:
         with open(file_path + file_name, "a+", encoding='utf-8') as f:
             for city in data_list:
-                f.write(city)
+                f.write(city + "\n")
     except OSError as reason:
         print(str(reason))
 
@@ -28,13 +28,12 @@ def get_city_list():
     city_list = []
     req = urllib.request.Request(search_url)
     resp = urllib.request.urlopen(req)
-    soup = BeautifulSoup(resp.read(), 'html.parser')
+    soup = BeautifulSoup(resp.read(), 'html.parser', from_encoding='"gb18030')
     newAlexa = soup.find(attrs={'id': 'newAlexa'})
     table = newAlexa.find('table')
     tr = table.findAll('a')
     for a in tr:
         city_list.append(base_url + a['href'])
-        print(base_url + a['href'])
     return city_list
 
 
@@ -43,26 +42,23 @@ def get_post_code(url):
     code_list = []
     req = urllib.request.Request(url)
     resp = urllib.request.urlopen(req)
-    soup = BeautifulSoup(resp.read(), 'html.parser')
-    table = soup.find('table', attrs={'class': 't12'})
-    tr = table.findAll('tr')
-    for i in tr:
-        print(i)
-        # if td[0].get_text() != '':
-        #     print(td[0].get_text() + ':' + td[1].get_text())
-        #     code_list.append())
-        # if td[3].get_text() != '':
-        #     code_list.append(td[3].get_text() + ':' + td[4].get_text() + ':' + td[5].get_text())
-        # return code_list
+    soup = BeautifulSoup(resp.read(), 'html.parser', from_encoding='"gb18030')
+    trs = soup.findAll('tr', attrs={'bgcolor': '#ffffff'})
+    for tr in trs:
+        td = tr.findAll("td")
+        if len(td) > 1:
+            code_list.append(td[0].get_text() + ':' + td[1].get_text() + ":" + td[2].get_text())
+            if len(str(td[3].get_text())) > 1:
+                code_list.append(td[3].get_text() + ':' + td[4].get_text() + ":" + td[5].get_text())
+    return code_list
 
 
 if __name__ == '__main__':
-    test_url = 'http://www.ip138.com/51/'
-    get_post_code(test_url)
-    # if not os.path.exists(file_path):
-    #     os.mkdir(file_path)
-    # if os.path.exists(file_path + file_name):
-    #     os.remove(file_path + file_name)
-    # city_list = get_city_list()
-    # for url in city_list:
-    #     write_to_file(get_post_code(url))
+    if not os.path.exists(file_path):
+        os.mkdir(file_path)
+    if os.path.exists(file_path + file_name):
+        os.remove(file_path + file_name)
+    city_list = get_city_list()
+    for url in city_list:
+        print("抓取: " + url)
+        write_to_file(get_post_code(url))
---
+++ README.md
@@ -0,0 +1,15 @@
+# ReptileSomething
+
+标签: 测试
+
+## 用Python抓点东西玩玩
+
+## 图片类
+
+- **GankPicCatch.py**：抓取gank.io所有的妹子图
+
+
+## 工具类
+
+- **CatchCityCode.py**：抓取所有的城市编码(天气查询用到)
+- **CatchPostCode.py**：抓取所有的邮政编码与电话区号
