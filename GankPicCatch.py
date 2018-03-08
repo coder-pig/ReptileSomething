f98ed8a5bb6bc519613aab9f8cdf2b541861cb11
CoderPig <779878443@qq.com>
2018-03-08 11:39:53 +0800

    Update Something.

--- GankPicCatch.py
+++ GankPicCatch.py
@@ -1,29 +1,52 @@
 # 爬取gank.io所有的妹子图
 
-import urllib.parse
-import json
+import requests
+import config as c
+import tools as t
+import multiprocessing
 
-import coderpig
 
-pic_save_path = "output/Picture/Gank/"
-api_gank_url = "http://gank.io/api/data/" + urllib.parse.quote("福利")
-pic_count = 58 * 10 + 5  # 所有的图片数
-max_count = 50  # 一页最多读取的数据量
+def download_pic(img_url):
+    try:
+        resp = requests.get(img_url, timeout=5)
+        if resp is not None:
+            print("下载图片:" + resp.request.url)
+            pic_name = img_url.split("/")[-1]
+            with open(c.GANK_PIC_SAVE_PATH + pic_name, "wb+") as f:
+                f.write(resp.content)
+            return None
+    except Exception as e:
+        pass
 
 
 def fetch_meizi_pic(url):
-    data = str(coderpig.get_resp(url).decode('utf-8'))
-    data = json.loads(data)
-    result_list = data['results']
-    for result in result_list:
-        coderpig.download_pic(result['url'], pic_save_path)
+    print("解析接口：" + url)
+    try:
+        resp = requests.get(url).json()
+        return resp['results']
+    except Exception as e:
+        print(e)
 
 
 if __name__ == '__main__':
-    coderpig.init_https()
-    coderpig.is_dir_existed(pic_save_path)
-    page = round(pic_count / max_count)  # 可读取页数，四舍五入
-    for i in range(1, int(page) + 1):
-        print("====== Download Page: ======= %d" % i)
-        fetch_url = api_gank_url + "/50/" + str(i)
-        fetch_meizi_pic(fetch_url)
+    t.is_dir_existed(c.GANK_PIC_SAVE_PATH)
+    t.is_dir_existed(c.GANK_PIC_URL_FILE_PATH)
+    print("检测图片图片url文件是否存在：")
+    if t.is_dir_existed(c.GANK_PIC_URL_FILE_PATH + c.GANK_PIC_URL_FILE_NAME, mkdir=False):
+        print("url文件已存在!")
+    else:
+        print("url文件不存在，开始解析图片接口...")
+        cur_page = 1
+        while True:
+            results = fetch_meizi_pic(c.GANK_PIC_API + str(cur_page))
+            if results is not None and len(results) > 0:
+                for result in results:
+                    t.write_str_data(result['url'], c.GANK_PIC_URL_FILE_PATH + c.GANK_PIC_URL_FILE_NAME)
+                cur_page += 1
+            else:
+                break
+        print("所有接口解析完毕！")
+    print("开始下载图片：")
+    url_list = t.load_list_from_file(c.GANK_PIC_URL_FILE_PATH + c.GANK_PIC_URL_FILE_NAME)
+    pool = multiprocessing.Pool()
+    pool.map(download_pic, url_list)
---
+++ config.py
@@ -0,0 +1,18 @@
+# 配置文件
+import os
+import urllib.parse
+
+# 常用User-Agent字典
+USER_AGENT_DICT = {
+    'chrome': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 '
+              'Safari/537.36',
+    'firefox': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:58.0) Gecko/20100101 Firefox/58.0'
+}
+
+# Gank 福利图配置
+GANK_PIC_API = "http://gank.io/api/data/" + urllib.parse.quote("福利") + "/50/"
+GANK_PIC_URL_FILE_NAME = "gank_pic_urls.txt"
+GANK_PIC_URL_FILE_PATH = os.getcwd() + "/out/file/Gank/"
+GANK_PIC_SAVE_PATH = os.getcwd() + "/out/Picture/Gank/"
+
+
