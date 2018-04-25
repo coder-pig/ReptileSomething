ae9b7506388e9509b670276df85f317458b19c44
CoderPig <779878443@qq.com>
2018-04-25 11:14:26 +0800

    更新机器人脚本，删除轮询，添加消息回复延时

--- code/tools/WechatXYZHelper.py
+++ code/tools/WechatXYZHelper.py
@@ -1,6 +1,8 @@
 # 微信小宇宙助手
 import datetime
 import re
+import time
+import random
 from threading import Timer
 
 import itchat
@@ -35,24 +37,15 @@ def xyz_reply(msg):
                 itchat.send('%s' % (msg['Content']), toUserName=group)
 
 
-# 每个半个小时发依次信息貌似能防止掉线
-def loop_send():
-    global count
-    itchat.send('大扎好，我系轱天乐，我四渣嘎辉，探挽懒月，介四里没有挽过的船新版本，'
-                '挤需体验三番钟，里造会干我一样，爱像借款游戏。'
-                , toUserName=itchat.search_chatrooms(name=u'探挽懒月')[0]['UserName'])
-    count += 1
-    if count < 10000:
-        Timer(1800, loop_send).start()
-
-
 # 自动通过加好友
 @itchat.msg_register(itchat.content.FRIENDS)
 def deal_with_friend(msg):
     if add_friend_compile.search(msg['Content']) is not None:
         itchat.add_friend(**msg['Text'])  # 自动将新好友的消息录入，不需要重载通讯录
+        time.sleep(random.randint(1, 3))
         itchat.send_msg('嘤嘤嘤，我是智障机器人小Pig，\n很高兴认识你，回复关键字:\n\n 加群，博客，Github，公众号，打赏 \n\n 来继续我们的摔跤♂故事！',
                         msg['RecommendInfo']['UserName'])
+        time.sleep(random.randint(1, 3))
         itchat.send_image('welcome.png', msg['RecommendInfo']['UserName'])
 
 
@@ -65,19 +58,27 @@ def deal_with_friend(msg):
 def deal_with_msg(msg):
     text = msg['Content']
     if text == u'加群':
+        time.sleep(random.randint(1, 3))
         itchat.add_member_into_chatroom(get_group_id("小猪的Python学习交流群"),
                                         [{'UserName': msg['FromUserName']}], useInvitation=True)
     elif text == u'博客':
+        time.sleep(random.randint(1, 3))
         return 'coder-pig的个人主页-掘金：https://juejin.im/user/570afb741ea493005de84da3'
     elif text == u'Github':
+        time.sleep(random.randint(1, 3))
         return 'https://github.com/coder-pig'
     elif text == u'公众号':
+        time.sleep(random.randint(1, 3))
         itchat.send_image('gzh.jpg', msg['FromUserName'])
     elif text == u'打赏':
+        time.sleep(random.randint(1, 3))
         itchat.send_image('ds.gif', msg['FromUserName'])
+        time.sleep(random.randint(1, 3))
         itchat.send_msg('您的打赏，会让小猪更有动力肝出\n更Interesting的文章，谢谢支持～', msg['FromUserName'])
+        time.sleep(random.randint(1, 3))
         itchat.send_image('wxpay.png', msg['FromUserName'])
     else:
+        time.sleep(random.randint(1, 3))
         itchat.send_image('hrwh.png', msg['FromUserName'])
 
 
@@ -89,6 +90,5 @@ def get_group_id(group_name):
 
 if __name__ == '__main__':
     count = 0
-    Timer(1800, loop_send).start()
     itchat.auto_login(enableCmdQR=2)
     itchat.run()
