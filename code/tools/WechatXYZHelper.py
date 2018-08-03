217115e0289ff2810d4134d19ba61b14cdb5dc63
coder- pig <779878443@qq.com>
2018-08-03 16:39:06 +0800

    fix:脚本更新

--- code/tools/WechatXYZHelper.py
+++ code/tools/WechatXYZHelper.py
@@ -31,7 +31,7 @@ def xyz_reply(msg):
             group_name.append(chat[0]['UserName'])
     # 过滤小宇宙新闻
     print(msg['ActualNickName'])
-    if msg['ActualNickName'] is not None and "小宇宙" in msg['ActualNickName']:
+    if msg['ActualNickName'] is not None and msg['ActualNickName'] == "小宇宙【早报定制】":
         result = xyz_compile.search(msg['Content'])
         if result is not None:
             if result.group() is not None:
@@ -47,8 +47,8 @@ def deal_with_friend(msg):
         if add_friend_compile.search(content.group(1)) is not None:
             itchat.add_friend(**msg['Text'])  # 自动将新好友的消息录入，不需要重载通讯录
             time.sleep(random.randint(1, 3))
-            itchat.send_msg('嘤嘤嘤，我是智障机器人小Pig，很高兴认识你，回复关键字:\n\n 加群，博客，Github，公众号，打赏 \n\n '
-                            '来继续我们的故♂事！\n( 可以别van♂我么，我只是个拉人机器人！！！)',
+            itchat.send_msg('嘤嘤嘤(˶ᵔᵕᵔ˶)，我是智障机器人小Pig，很高兴认识你，回复关键字:\n\n 加群，博客，Github，公众号，打赏，小猪 \n\n '
+                            '来继续我们的故♂事！\n( 别van♂我哦，我只是个拉人机器人，没有其他附加功能，不会聊天哦！！！)',
                             msg['RecommendInfo']['UserName'])
             time.sleep(random.randint(1, 3))
             itchat.send_image('welcome.png', msg['RecommendInfo']['UserName'])
@@ -64,8 +64,7 @@ def deal_with_msg(msg):
     text = msg['Content']
     if text == u'加群':
         time.sleep(random.randint(1, 3))
-        itchat.add_member_into_chatroom(get_group_id("小猪的Python学习交流群"),
-                                        [{'UserName': msg['FromUserName']}], useInvitation=True)
+        itchat.send_msg('(｀･ω･´)ゞ非常抱歉，微信粑粑把拉人接口禁掉了，只能等小猪童鞋手动拉你啦！ヾﾉ≧∀≦)o 滑♂稽！', msg['FromUserName'])
     elif text == u'博客':
         time.sleep(random.randint(1, 3))
         return 'coder-pig的个人主页-掘金：https://juejin.im/user/570afb741ea493005de84da3'
@@ -79,9 +78,14 @@ def deal_with_msg(msg):
         time.sleep(random.randint(1, 3))
         itchat.send_image('ds.gif', msg['FromUserName'])
         time.sleep(random.randint(1, 3))
-        itchat.send_msg('您的打赏，会让小猪更有动力肝出\n更Interesting的文章，谢谢支持～', msg['FromUserName'])
+        itchat.send_msg('(˶ᵔᵕᵔ˶)您的打赏，会让小猪更有动力肝♂出\n更Interesting的文章，谢谢支持～', msg['FromUserName'])
         time.sleep(random.randint(1, 3))
         itchat.send_image('wxpay.png', msg['FromUserName'])
+    elif text == u'小猪':
+        time.sleep(random.randint(1, 3))
+        itchat.send_msg('(˶ᵔᵕᵔ˶)小猪童鞋不闲聊哦，有问题欢迎到群里讨论哦~', msg['FromUserName'])
+        time.sleep(random.randint(1, 3))
+        itchat.send_image('scan_code.png', msg['FromUserName'])
     else:
         time.sleep(random.randint(1, 3))
         itchat.send_image('hrwh.png', msg['FromUserName'])
